b1493a05c2ee8e787225692e6b91de3e53c32f0a
xaven <2274420416@qq.com>
2018-08-06 20:30:40 +0800

    测试-------

--- dubbo_provider/src/main/java/com/xgj/service/TestServiceImpl.java
+++ dubbo_provider/src/main/java/com/xgj/service/TestServiceImpl.java
@@ -4,7 +4,7 @@ import com.xgj.entity.Demo;
 
 public class TestServiceImpl implements  TestService {
     public String test(Demo demo) {
-        System.out.println("!!!--!");
+        System.out.println("!!!--!11");
         System.out.println(demo.toString());
         System.out.println("ahadhiad ");
         return demo.toString();
